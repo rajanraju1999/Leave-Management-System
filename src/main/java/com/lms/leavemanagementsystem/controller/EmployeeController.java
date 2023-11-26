e9a3fdc2a980a2cbfee6703010ece81e4f4b9432
nhemanthkumar <62937697+nhemanthkumar@users.noreply.github.com>
2023-11-26 18:01:07 +0530

    added @Requestmapping annotation in employee controller

--- src/main/java/com/lms/leavemanagementsystem/controller/EmployeeController.java
+++ src/main/java/com/lms/leavemanagementsystem/controller/EmployeeController.java
@@ -1,7 +1,11 @@
 package com.lms.leavemanagementsystem.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
+@RequestMapping("/user")
 public class EmployeeController {
+    
 }
